2db4fb82d348d882097480de40af1b0759804a35
Li Gen <ligen0018@outlook.com>
2020-11-17 21:34:47 +0800

    mine

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
+
+// 连接数据库
+mongoose.connect("mongodb://localhost:27017/db", { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+  if (error) {
+    console.log("数据库连接失败");
+  } else {
+    console.log("数据库连接成功");
+
+    // 启动服务器
+    const server = http.createServer(app.callback());
+    server.on("error", (error) => {
+      console.log("服务启动失败");
+    });
+    server.listen(3000, "localhost", () => {
+      console.log("服务启动成功");
+    });
+  }
+});
